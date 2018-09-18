72b4eef211f96a25316eece0d128e3273865d6e9
Shashwat Black <shashwat.black@gmail.com>
2018-09-17 20:43:10 -0500

    Moved from local storage to indexed db for logging.

--- background.js
+++ background.js
@@ -7,33 +7,49 @@ function init() {
     });
 }
 
+var db;
+
 // init data stores
 function initDataStores() {
-    initUsageData = new Promise(function(resolve, reject) {
-        chrome.storage.local.get(['usage_data'], function(response) { 
-            if (!response.usage_data) {
-                chrome.storage.local.set({usage_data: []}, function() {
-                    resolve();
-                });
-            } else {
-                resolve();
-            }
-        });
-    });
-    
-    // add more data stores as needed here
 
-    return Promise.all([initUsageData]);
+    const dbPromise = idb.open('mobbedGlob', 1, upgradeDB => {
+        console.log('running onupgradeneeded');
+        if (!upgradeDB.objectStoreNames.contains('usageStore')) {
+            usageStore = upgradeDB.createObjectStore('usageStore',
+                {keyPath: 'id', autoIncrement: true}
+            );
+            usageStore.createIndex('tabId', 'tabId', {unique: false});
+        }
+        // add more data stores as needed
+    });
+
+    dbPromise.then(function(resolved_db) {
+        db = resolved_db;
+        console.log('running onsuccess');
+    }).catch(function(e) {
+        console.error('onerror!');
+        console.dir(e);
+    });
+
+    return Promise.all([dbPromise]);
 }
 
 function addUsage(action, tabId, params) {
-    chrome.storage.local.get(['usage_data'], function(response) { 
-        response.usage_data.push({
-            action: action,
-            tabId: tabId,
-            params: params
-        });
-        chrome.storage.local.set({usage_data: response.usage_data});
+    var transaction = db.transaction(['usageStore'], 'readwrite');
+    var usageStore = transaction.objectStore('usageStore');
+    var item = {
+        action: action,
+        tabId: tabId
+    };
+
+    for (let p in params) {
+        item[p] = params[p];
+    }
+
+    var request = usageStore.add(item).then(function(e) {
+        console.log('Woot! Did it');
+    }).catch(function(e) {
+        console.error('Error', e.name, e.message);
     });
 }
 
@@ -41,12 +57,12 @@ function addUsage(action, tabId, params) {
 function addListeners() {
     chrome.tabs.onCreated.addListener(function(tab) {
         // find tuple with same id which is active
-        addUsage("created", tab.id);
+        addUsage("created", tab.id, {url: tab.url, sessionId: tab.sessionId});
     });
 
     chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         if (changeInfo.status !== 'complete') return;
-        addUsage("updated", tab.id, {url: tab.url});
+        addUsage("updated", tab.id, {url: tab.url, sessionId: tab.sessionId});
     });
 
     chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
---
+++ sandbox.js
@@ -0,0 +1,76 @@
+var db;
+
+upgradeNeeded = function(e) {
+  var db = e.target.result;
+  console.log('running onupgradeneeded');
+  if (!db.objectStoreNames.contains('store')) {
+    var storeOS = db.createObjectStore('store',
+      {keyPath: 'name'});
+  }
+};
+
+var openRequest = idb.open('test_db', 1, upgradeNeeded);
+
+openRequest.then(function(e) {
+  console.log('running onsuccess');
+  db = e.target.result;
+  addItem();
+}).catch(function(e) {
+  console.log('onerror!');
+  console.dir(e);
+});
+
+function addItem() {
+  var transaction = db.transaction(['store'], 'readwrite');
+  var store = transaction.objectStore('store');
+  var item = {
+    name: 'apples',
+    price: '$3.99',
+    description: 'It is a yellow apple!',
+    created: new Date().getTime()
+  };
+
+ var request = store.add(item).then(function(e) {
+    console.log('Woot! Did it');
+  }).catch(function(e) {
+    console.log('Error', e.target.error.name);
+  });
+}
+
+
+
+-------------------------------------------------
+var transaction = db.transaction(['usageStore'], 'readwrite');
+var usageStore = transaction.objectStore('usageStore');
+usageStore.getAll().then(function(val) {console.log(val);});
+-------------------------------------------------
+var transaction = db.transaction(['usageStore'], 'readwrite');
+var usageStore = transaction.objectStore('usageStore');
+var usageIndex = usageStore.index('tabId');
+usageIndex.getAll(IDBKeyRange.bound(100, 500)).then(function(v) { console.log(v); })
+-------------------------------------------------
+All keys ≤ x           IDBKeyRange.upperBound(x)
+All keys < x           IDBKeyRange.upperBound(x, true)
+All keys ≥ y           IDBKeyRange.lowerBound(y)
+All keys > y           IDBKeyRange.lowerBound(y, true)
+All keys ≥ x && ≤ y    IDBKeyRange.bound(x, y)
+All keys > x &&< y     IDBKeyRange.bound(x, y, true, true)
+All keys > x && ≤ y    IDBKeyRange.bound(x, y, true, false)
+All keys ≥ x &&< y     IDBKeyRange.bound(x, y, false, true)
+The key = z            IDBKeyRange.only(z)
+-------------------------------------------------
+usageStore /
+var transaction = db.transaction(['usageStore'], 'readwrite');
+var usageStore = transaction.objectStore('usageStore');
+var usageIndex = usageStore.index('tabId');
+usageIndex.openCursor(IDBKeyRange.bound(100, 500)).then(function cursorCallback(cursor) {
+  if (!cursor) return;
+  console.log(cursor.value); 
+  return cursor.continue().then(cursorCallback);
+}).then(function() { console.log("Done!"); });
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
+-------------------------------------------------
